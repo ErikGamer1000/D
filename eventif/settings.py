c539282d41bea880facf81665b4700fd50d103b5
ErikGamer1000 <erikmiluk@gmail.com>
2024-08-26 15:35:47 -0300

    Allowed hosts

--- eventif/settings.py
+++ eventif/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,8 +28,7 @@ SECRET_KEY = "django-insecure-ye_8enc^(u=0rnbkvaw*#tkue*#q7c@floula$(u0wn5p!q7uz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 # Application definition
 
