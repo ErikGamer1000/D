ff779e344864d0d9c7eccdb8172d001996bfeca2
ErikGamer1000 <erikmiluk@gmail.com>
2024-11-18 15:48:15 -0300

    Thank you page

--- subscriptions/tests/test_view_subscribe.py
+++ subscriptions/tests/test_view_subscribe.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core import mail
 from subscriptions.forms import SubscriptionForm
 from subscriptions.models import Subscription
+import unittest
 
 class SubscribeGet(TestCase):
     def setUp(self):
@@ -32,8 +33,9 @@ class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name=" Erik Pinheiro", cpf="12345678901", email="erikmiluk@gmail.com", phone="53-12345-6789")
         self.resp = self.client.post('/inscricao/', data)
+
     def test_post(self):
-        self.assertEqual(302, self.resp.status_code)
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscription_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -56,9 +58,3 @@ class SubscribePostInvalid(TestCase):
         self.assertTrue(form.errors)
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name=" Erik Pinheiro", cpf="12345678901", email="erikmiluk@gmail.com", phone="53-12345-6789")
-        resp = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(resp, 'Inscrição realizada com sucesso!')
