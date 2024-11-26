91a261ab6403bc60c69cfce6a5385f511890cba8
ErikGamer1000 <erikmiluk@gmail.com>
2024-11-26 14:37:55 -0300

    Trabalho do terceiro bimestre realizado.

--- contact/tests.py
+++ contact/tests.py
@@ -10,34 +10,42 @@ class ContactTestForm(TestCase):
         fields = ['name', 'phone', 'email', 'message']
         self.assertSequenceEqual(fields, list(self.form.fields))
 
-# class ContactPostValid(TestCase):
-#     def setUp(self):
-#         data = dict(name="Erik Miluk Pinheiro", email="erikmiluk@gmail.com", phone="53-91234-5678", message="Mensagem padrão de envio de contato")
-#         self.client.post('/contato/', data)
-#         self.email = mail.outbox[0]
+class ContactGet(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/contato/')
 
-#     def test_contact_email_subject(self):
-#         expect = 'Confirmação de contato!'
-#         self.assertEqual(expect, self.email.subject)
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
 
-#     def test_contact_email_from(self):
-#         expect = 'erikmiluk@gmail.com'
-#         self.assertEqual(expect, self.email.from_email)
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'contact/contact_form.html')
 
-#     def test_contact_email_to(self):
-#         expect = ['contato@eventif.com.br', 'erikmiluk@gmail.com']
-#         self.assertEqual(expect, self.email.to)
+    def test_html(self):
+        tags = (
+            ('<form', 1),
+            ('<input', 5),
+            ('type="text"', 2),
+            ('type="email"', 1),
+            ('type="submit"', 1),
+            ('<textarea', 1)
+        )
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
 
-#     def test_contact_email_body(self):
-#         subjects = (
-#             'Erik Miluk Pinheiro',
-#             'erikmiluk@gmail.com',
-#             '53-91234-5678',
-#             'Mensagem padrão de envio de contato'
-#         )
-#         for content in subjects:
-#             with self.subTest():
-#                 self.assertIn(content, self.email.body)
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+class ContactPostValid(TestCase):
+    def setUp(self):
+        data = dict(name="Erik Pinheiro", phone="53-91234-5678", email="erikmiluk@gmail.com", message="Contato enviado com sucesso.")
+        self.resp = self.client.post('/contato/', data)
+
+    def test_post(self):
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_send_contact_email(self):
+        self.assertEqual(1, len(mail.outbox))
 
 class ContactPostInvalid(TestCase):
     def setUp(self):
@@ -85,4 +93,10 @@ class ContactEmailSent(TestCase):
         )
         for content in subjects:
             with self.subTest():
-                self.assertIn(content, self.email.body)        
+                self.assertIn(content, self.email.body) 
+
+class SuccessContactMessage(TestCase):
+    def test_success_message(self):
+        data = dict(name="Erik Miluk Pinheiro", email="erikmiluk@gmail.com", phone="53-91234-5678", message="Necessito entrar em contato sobre uma dúvida no evento")
+        resp = self.client.post('/contato/', data, follow=True)
+        self.assertContains(resp, 'Contato enviado')
